1870518dc818f787da0ea1c3453a81167bd24f0c
jamie denvir <jwdenvir@gmail.com>
2021-05-06 18:41:09 -0400

    makes some routes to display JSON for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,17 @@ class ProductsController < ApplicationController
   def all_products
     products = Product.all
     render json: {message: "All Products"}
-    
   end
 
+  def first_product
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def second_product
+    product = Product.second
+    render json: product.as_json
+    
+  end
 
 end
